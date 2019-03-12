570f61a96ac5712f9eb5e446015dbc156f55dd71
yaukie <yaukie@163.com>
2019-03-12 15:38:28 +0800

    修改rest服务类

--- src/main/org/yaukie/soap/ProductService.java
+++ src/main/org/yaukie/soap/ProductService.java
@@ -15,12 +15,12 @@ import org.yaukie.soap.bean.Product;
 @Soap(serverPath="product",serviceType=Soap.Type.Restful)
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductService {
-	private List list = new ArrayList();
 	
     @GET
     @Path("/getProductXML")
     @Produces(MediaType.APPLICATION_ATOM_XML)
     public List<Product> getProductXML() {
+    	  List list = new ArrayList();
     	Product pro = new Product();
     	pro.setProName("苹果");
     	pro.setPrice(43);
@@ -28,17 +28,17 @@ public class ProductService {
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro1 = new Product();
     	pro.setProName("苹果1");
-    	pro.setPrice(43);
+    	pro.setPrice(3);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro2 = new Product();
     	pro.setProName("苹果2");
-    	pro.setPrice(43);
+    	pro.setPrice(23);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro3 = new Product();
     	pro.setProName("苹果3");
-    	pro.setPrice(43);
+    	pro.setPrice(52);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	list.add(pro);
@@ -51,8 +51,9 @@ public class ProductService {
  
     @GET
     @Path("/getProductJSON")
-    @Produces(MediaType.APPLICATION_ATOM_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Product> getProductJSON() {
+    	  List list = new ArrayList();
     	Product pro = new Product();
     	pro.setProName("苹果");
     	pro.setPrice(43);
@@ -60,17 +61,17 @@ public class ProductService {
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro1 = new Product();
     	pro.setProName("苹果1");
-    	pro.setPrice(43);
+    	pro.setPrice(44);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro2 = new Product();
     	pro.setProName("苹果2");
-    	pro.setPrice(43);
+    	pro.setPrice(55);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	Product pro3 = new Product();
     	pro.setProName("苹果3");
-    	pro.setPrice(43);
+    	pro.setPrice(66);
     	pro.setType("fruit");
     	pro.setExpire(System.currentTimeMillis()+4000);
     	list.add(pro);
